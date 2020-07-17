13c236f1f374dc076654e14d2322e7a0803c2fbd
Y-poi-sir <66407413+Y-poi-sir@users.noreply.github.com>
2020-07-17 11:37:00 +0800

    Update img_arr.js

--- img_arr.js
+++ img_arr.js
@@ -1,4 +1,4 @@
 export default{
-    img1:require('./bighead.png'),
-    img2:require('./background1.jpg'),
+    img1:require('./imgBackground/bighead.png'),
+    img2:require('./imgBackground/background1.jpg'),
 };
